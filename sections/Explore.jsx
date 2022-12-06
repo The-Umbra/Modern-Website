37acaf27a9ddc585f70c835662e27a002c056240
Furkan <gami.furkan@gmail.com>
2022-12-06 17:23:32 +0300

    Started to make Explore section

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { staggerContainer } from '../utils/motion';
+import { ExploreCard, TypingText, TitleText } from '../components';
+import { exploreWorlds } from "../constants"
+
 const Explore = () => (
   <section>
     Explore section
